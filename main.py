ae7f17799adf72ab85b2fe8c226b0a7271025cbc
abdallah1097 <abdallah.wallyallah97@gmail.com>
2025-04-01 02:17:13 +0400

    Add model loading

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from transformers import AutoModelForCausalLM, AutoProcessor
+
 if __name__ == "__main__":
-    print(f"App ran successfully")
+    model_id = "microsoft/Florence-2-large"
+
+    # Ensure the runtime is set to GPU in Colab.
+    model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, torch_dtype="auto").eval().cuda()
+    processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
