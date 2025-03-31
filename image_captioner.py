854354ba0aa3e344528106fbb3f41fcb4d7d2f34
abdallah1097 <abdallah.wallyallah97@gmail.com>
2025-04-01 02:44:01 +0400

    Add class attributes

--- image_captioner.py
+++ image_captioner.py
@@ -1,10 +1,21 @@
 from transformers import AutoModelForCausalLM, AutoProcessor
+import random
+import cv2
+import torch
+from PIL import Image
+from ultralytics.utils.downloads import safe_download
+from ultralytics.utils.plotting import Annotator, colors
+import os
 
 
-class image_captioner:
-    def __init__(self):
-        model_id = "microsoft/Florence-2-large"
+class ImageCaptioner:
+    def __init__(self, model_id, max_new_tokens, num_beams):
+        # Set Class Attributes
+        self.model_id = model_id
+        self.max_new_tokens = max_new_tokens
+        self.num_beams = num_beams
 
-        # Ensure the runtime is set to GPU in Colab.
-        model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, torch_dtype="auto").eval().cuda()
-        processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
+        # Load Model and Processor
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_id, trust_remote_code=True, torch_dtype="auto").eval().cuda()
+        self.processor = AutoProcessor.from_pretrained(self.model_id, trust_remote_code=True)
+
--- main.py
+++ main.py
@@ -2,4 +2,26 @@ from image_captioner import ImageCaptioner
 
 
 if __name__ == "__main__":
-    image_captioner = ImageCaptioner()
+    # Define Arguments
+    # TODO: To be moved to run script
+    model_id = "microsoft/Florence-2-large"
+    max_new_tokens=1024  # Maximum number of tokens to generate
+    num_beams=3  # Beam search width for better predictions
+    input_image_filepath = None
+    save_results = True
+
+    # Create Instance of Image Captioner
+    image_captioner = ImageCaptioner(
+        model_id=model_id,
+        max_new_tokens=max_new_tokens,
+        num_beams=num_beams,
+    )
+
+    # Infer the Image Captioner
+    prompt = "<DENSE_REGION_CAPTION>"
+
+    image_captioner.infer(
+        input_image_filepath=input_image_filepath,
+        prompt=prompt,
+        save_results=save_results,
+    )
