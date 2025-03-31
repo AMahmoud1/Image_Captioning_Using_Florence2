5fbb8be4f0b6e6adaeced4c6a56d587855bc5e00
abdallah1097 <abdallah.wallyallah97@gmail.com>
2025-04-01 03:07:17 +0400

    Flake8 and Isort fixes

--- image_captioner.py
+++ image_captioner.py
@@ -1,11 +1,12 @@
-from transformers import AutoModelForCausalLM, AutoProcessor
+import os
 import random
+
 import cv2
 import torch
 from PIL import Image
+from transformers import AutoModelForCausalLM, AutoProcessor
 from ultralytics.utils.downloads import safe_download
 from ultralytics.utils.plotting import Annotator, colors
-import os
 
 
 class ImageCaptioner:
@@ -16,8 +17,16 @@ class ImageCaptioner:
         self.num_beams = num_beams
 
         # Load Model and Processor
-        self.model = AutoModelForCausalLM.from_pretrained(self.model_id, trust_remote_code=True, torch_dtype="auto").eval().cuda()
-        self.processor = AutoProcessor.from_pretrained(self.model_id, trust_remote_code=True)
+        self.model = (
+            AutoModelForCausalLM.from_pretrained(
+                self.model_id, trust_remote_code=True, torch_dtype="auto"
+            )
+            .eval()
+            .cuda()
+        )
+        self.processor = AutoProcessor.from_pretrained(
+            self.model_id, trust_remote_code=True
+        )
 
     def infer(self, input_image_filepath, prompt, save_results):
         """
@@ -38,12 +47,16 @@ class ImageCaptioner:
             text=prompt,  # Text input for the model
             images=image,  # Image input for the model
             return_tensors="pt",  # Return PyTorch tensors
-        ).to("cuda", torch.float16)  # Move inputs to GPU with float16 precision
+        ).to(
+            "cuda", torch.float16
+        )  # Move inputs to GPU with float16 precision
 
         # Generate model predictions (token IDs)
         generated_ids = self.model.generate(
             input_ids=inputs["input_ids"].cuda(),  # Convert text input IDs to CUDA
-            pixel_values=inputs["pixel_values"].cuda(),  # Convert image pixel values to CUDA
+            pixel_values=inputs[
+                "pixel_values"
+            ].cuda(),  # Convert image pixel values to CUDA
             max_new_tokens=self.max_new_tokens,  # Set maximum number of tokens
             early_stopping=False,  # Disable early stopping
             do_sample=False,  # Use deterministic inference
@@ -54,13 +67,18 @@ class ImageCaptioner:
         generated_text = self.processor.batch_decode(
             generated_ids,  # Generated token IDs
             skip_special_tokens=False,  # Retain special tokens in output
-        )[0]  # Extract first result from batch
+        )[
+            0
+        ]  # Extract first result from batch
 
         # Post-process the generated text into a structured response
         parsed_answer = self.processor.post_process_generation(
             generated_text,  # Raw generated text
             task=prompt,  # Task type for post-processing
-            image_size=(image.width, image.height),  # Original image dimensions for scaling output
+            image_size=(
+                image.width,
+                image.height,
+            ),  # Original image dimensions for scaling output
         )[prompt]
 
         # Save Results
@@ -69,10 +87,7 @@ class ImageCaptioner:
                 image=image,
                 results=parsed_answer,
             )
-            self.save_image(
-                result_image=annotator.result(),
-                path="output.jpg"
-            )
+            self.save_image(result_image=annotator.result(), path="output.jpg")
         return parsed_answer
 
     def read_image(self, filename):
@@ -97,11 +112,16 @@ class ImageCaptioner:
 
             if not os.path.isfile(image_path):
                 # Download the image if not exist
-                safe_download(f"https://github.com/ultralytics/assets/releases/download/v0.0.0/{image_name}", image_path)
+                safe_download(
+                    f"https://github.com/ultralytics/assets/releases/download/v0.0.0/{image_name}",
+                    image_path,
+                )
 
         # Read the image using OpenCV and convert it into the PIL format
-        return Image.fromarray(cv2.cvtColor(cv2.imread(f"{image_path}"), cv2.COLOR_BGR2RGB))
-    
+        return Image.fromarray(
+            cv2.cvtColor(cv2.imread(f"{image_path}"), cv2.COLOR_BGR2RGB)
+        )
+
     def annotate(self, image, results):
         """
         Annotates an image with bounding boxes and labels using the Ultralytics annotator.
@@ -121,7 +141,7 @@ class ImageCaptioner:
         for idx, (box, label) in enumerate(zip(results["bboxes"], results["labels"])):
             annotator.box_label(box, label=label, color=colors(idx, True))
         return annotator
-    
+
     def save_image(self, result_image, path):
         """
         Saves an image to the specified file path.
@@ -135,5 +155,5 @@ class ImageCaptioner:
         Returns:
             None
         """
-        output_image = Image.fromarray(result_image)  
+        output_image = Image.fromarray(result_image)
         output_image.save(path)  # Save the image
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import argparse
-from image_captioner import ImageCaptioner
 
+from image_captioner import ImageCaptioner
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Image Captioning")
 
-    parser.add_argument("--model_id", type=str, default="microsoft/Florence-2-large", help="Model ID")
-    parser.add_argument("--max_new_tokens", type=int, default=1024, help="Maximum number of tokens to generate")
+    parser.add_argument(
+        "--model_id", type=str, default="microsoft/Florence-2-large", help="Model ID"
+    )
+    parser.add_argument(
+        "--max_new_tokens",
+        type=int,
+        default=1024,
+        help="Maximum number of tokens to generate",
+    )
     parser.add_argument("--num_beams", type=int, default=3, help="Beam search width")
-    parser.add_argument("--input_image_filepath", type=str, required=True, help="Path to the input image")
-    parser.add_argument("--save_results", type=bool, default=True, help="Whether to save the results")
+    parser.add_argument(
+        "--input_image_filepath",
+        type=str,
+        required=True,
+        help="Path to the input image",
+    )
+    parser.add_argument(
+        "--save_results", type=bool, default=True, help="Whether to save the results"
+    )
 
     args = parser.parse_args()
 
@@ -24,7 +38,9 @@ if __name__ == "__main__":
     prompt = "<DENSE_REGION_CAPTION>"
 
     image_captioner.infer(
-        input_image_filepath=None if args.input_image_filepath == "None" else args.input_image_filepath,
+        input_image_filepath=(
+            None if args.input_image_filepath == "None" else args.input_image_filepath
+        ),
         prompt=prompt,
         save_results=args.save_results,
     )
