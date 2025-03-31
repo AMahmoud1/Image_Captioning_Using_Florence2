79b9f6206ef06520e3c4bc876772706ba2441a3e
abdallah1097 <abdallah.wallyallah97@gmail.com>
2025-04-01 02:55:48 +0400

    Move args to bash script

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
+import argparse
 from image_captioner import ImageCaptioner
 
 
 if __name__ == "__main__":
-    # Define Arguments
-    # TODO: To be moved to run script
-    model_id = "microsoft/Florence-2-large"
-    max_new_tokens=1024  # Maximum number of tokens to generate
-    num_beams=3  # Beam search width for better predictions
-    input_image_filepath = None
-    save_results = True
+    parser = argparse.ArgumentParser(description="Run Image Captioning")
+
+    parser.add_argument("--model_id", type=str, default="microsoft/Florence-2-large", help="Model ID")
+    parser.add_argument("--max_new_tokens", type=int, default=1024, help="Maximum number of tokens to generate")
+    parser.add_argument("--num_beams", type=int, default=3, help="Beam search width")
+    parser.add_argument("--input_image_filepath", type=str, required=True, help="Path to the input image")
+    parser.add_argument("--save_results", type=bool, default=True, help="Whether to save the results")
+
+    args = parser.parse_args()
 
     # Create Instance of Image Captioner
     image_captioner = ImageCaptioner(
-        model_id=model_id,
-        max_new_tokens=max_new_tokens,
-        num_beams=num_beams,
+        model_id=args.model_id,
+        max_new_tokens=args.max_new_tokens,
+        num_beams=args.num_beams,
     )
 
     # Infer the Image Captioner
     prompt = "<DENSE_REGION_CAPTION>"
 
     image_captioner.infer(
-        input_image_filepath=input_image_filepath,
+        input_image_filepath=None if args.input_image_filepath == "None" else args.input_image_filepath,
         prompt=prompt,
-        save_results=save_results,
+        save_results=args.save_results,
     )
--- run.sh
+++ run.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
+# Default values
+MODEL_ID="microsoft/Florence-2-large"
+MAX_NEW_TOKENS=1024
+NUM_BEAMS=3
+INPUT_IMAGE=None  # Default to None
+SAVE_RESULTS=true
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --model-id)
+            MODEL_ID="$2"
+            shift 2
+            ;;
+        --max-new-tokens)
+            MAX_NEW_TOKENS="$2"
+            shift 2
+            ;;
+        --num-beams)
+            NUM_BEAMS="$2"
+            shift 2
+            ;;
+        --input-image)
+            INPUT_IMAGE="$2"
+            shift 2
+            ;;
+        --save-results)
+            SAVE_RESULTS="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
 # Name of the Conda virtual environment
 VENV_NAME="image_captioning_venv"
 
@@ -20,6 +58,11 @@ source activate $VENV_NAME || conda activate $VENV_NAME
 pip install transformers==4.49.0 ultralytics einops timm
 
 # Run The Application
-echo "Image Captioning Application is Starting..."
-python3 main.py
-echo "Image Captioning Application has Finished"
+echo "Running Image Captioning with:"
+echo "  Model ID: $MODEL_ID"
+echo "  Max New Tokens: $MAX_NEW_TOKENS"
+echo "  Num Beams: $NUM_BEAMS"
+echo "  Input Image: $INPUT_IMAGE"
+echo "  Save Results: $SAVE_RESULTS"
+
+python3 main.py --model_id "$MODEL_ID" --max_new_tokens "$MAX_NEW_TOKENS" --num_beams "$NUM_BEAMS" --input_image_filepath "$INPUT_IMAGE" --save_results "$SAVE_RESULTS"
